7a9fee4a4e052ff1aca1b35ba537410a85af0c1a
choiseonkang <com1368@naver.com>
2024-12-01 10:55:59 +0900

    배치 초기화 및 실행 config 작성

---
+++ src/main/java/dev/fastcampus/restaurantbatch/config/BatchConfig.java
@@ -0,0 +1,7 @@
+package dev.fastcampus.restaurantbatch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+
+@EnableBatchProcessing
+public class BatchConfig {
+}
