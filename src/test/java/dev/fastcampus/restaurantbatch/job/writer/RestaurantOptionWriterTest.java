52878902084cbff3c31a5a5b724d48e73866310d
choiseonkang <com1368@naver.com>
2024-12-01 18:27:46 +0900

    음식점 부가정보 writer 테스트 코드 작성

---
+++ src/test/java/dev/fastcampus/restaurantbatch/job/writer/RestaurantOptionWriterTest.java
@@ -0,0 +1,34 @@
+package dev.fastcampus.restaurantbatch.job.writer;
+
+import dev.fastcampus.restaurantbatch.job.dto.RestaurantCsvDto;
+import dev.fastcampus.restaurantbatch.model.RestaurantOption;
+import dev.fastcampus.restaurantbatch.repository.InMemoryRestaurantOptionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.Chunk;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RestaurantOptionWriterTest {
+
+    private InMemoryRestaurantOptionRepository restaurantOptionRepository;
+    public RestaurantOptionWriter restaurantOptionWriter;
+
+    @BeforeEach
+    void setUp() {
+        restaurantOptionRepository = new InMemoryRestaurantOptionRepository();
+        restaurantOptionWriter = new RestaurantOptionWriter(restaurantOptionRepository);
+    }
+
+    @Test
+    void 음식점_주소_정보_저장() throws Exception {
+        restaurantOptionWriter.write(Chunk.of(RestaurantCsvDto.builder().waterSupplyTypeName("상수도전용").build()));
+
+        List<RestaurantOption> restaurantOptions = restaurantOptionRepository.findMany();
+
+        assertThat(restaurantOptions.size()).isEqualTo(1);
+        assertThat(restaurantOptions.get(0).getWaterSupplyTypeName()).isEqualTo("상수도전용");
+    }
+}
