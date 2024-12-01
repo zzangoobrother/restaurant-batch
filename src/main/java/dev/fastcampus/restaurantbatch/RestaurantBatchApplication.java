be126e415cc98da37fc3fd77cb8bb17b0fd8a3dd
choiseonkang <com1368@naver.com>
2024-12-01 10:24:58 +0900

    init 프로젝트 시작

---
+++ src/main/java/dev/fastcampus/restaurantbatch/RestaurantBatchApplication.java
@@ -0,0 +1,13 @@
+package dev.fastcampus.restaurantbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fastcampus/restaurantbatch/RestaurantBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fastcampus.restaurantbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
